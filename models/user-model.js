004ac5939ec9590ade1252462301e10dcec93fc1
nileshmk <nkeshavdas@gmail.com>
2019-03-07 18:54:56 +0530

    orchid

--- models/user-model.js
+++ models/user-model.js
@@ -10,24 +10,7 @@ const userSchema = mongoose.Schema({
     
     _id: String,
     firstName: String,
-    lastName: String,
-    email: String,
-    password: String,
-    profileURL: String,
-    profileType:{
-        type: String,
-        enum:['google','normal','facebook']
-    },
-    profilePhoto:{
-        data:Buffer,
-        contentType: String
-    },
-    employment: {type: String, default: null},
-    education: {type: String, default: null},
-    collegeName: {type: String, default: null},
-    location: {type: String, default: null},
-    description: {type: String, default: null}
-    
+    lastName: String
 });
 
 const User = mongoose.model('user', userSchema);
